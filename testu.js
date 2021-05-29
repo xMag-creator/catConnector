44cc742dd51be6e089bdb62966d7da0b7dfb735b
Przemysław Kobylarz <p.kobylarz@outlook.com>
2021-05-29 13:58:19 +0200

    two cats added

---
+++ testu.js
@@ -0,0 +1,3 @@
+//onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)}
+//onclick = function (e){console.log("mouse location:", e.clientX, e.clientY)}
+ondrag = function (e){console.log("mouse location:", e.clientX, e.clientY)}
